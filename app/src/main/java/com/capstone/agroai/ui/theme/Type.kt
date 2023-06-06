1cfa6469d2736aee1bde59afce6f2cd9ca5375df
Xenovka <wendyanto18@gmail.com>
2023-06-06 13:05:46 +0700

    required font family

--- app/src/main/java/com/capstone/agroai/ui/theme/Type.kt
+++ app/src/main/java/com/capstone/agroai/ui/theme/Type.kt
@@ -2,11 +2,27 @@ package com.capstone.agroai.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.capstone.agroai.R
 
 // Set of Material typography styles to start with
+val Libre = FontFamily(
+    Font(R.font.librebaskerville_regular, FontWeight.Normal),
+    Font(R.font.librebaskerville_bold, FontWeight.Bold),
+    Font(R.font.librebaskerville_italic, style = FontStyle.Italic),
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
